f81d572f5176ecbb1fbedace2288998ea20e81ee
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-08-23 20:42:56 +0300

    Add algorithm: Subsets

---
+++ src/main/java/Algorithms/Backtracking/Subsets.java
@@ -0,0 +1,24 @@
+package Algorithms.Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/subsets/
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        backtrack(res, new ArrayList(), nums, 0);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, int start)  {
+        res.add(new ArrayList(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(res, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
