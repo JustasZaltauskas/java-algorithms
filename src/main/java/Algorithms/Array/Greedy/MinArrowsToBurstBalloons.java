3effc75b344a84cbaa8c93e03e65a0adafa691c7
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-09-13 10:06:40 +0300

    Improve algorithm: Minimum Number of Arrows to Burst Balloons

--- src/main/java/Algorithms/Array/Greedy/MinArrowsToBurstBalloons.java
+++ src/main/java/Algorithms/Array/Greedy/MinArrowsToBurstBalloons.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 
 // https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
 public class MinArrowsToBurstBalloons {
-    public static int find(int[][] points) {
+    public int findMinArrowShots(int[][] points) {
         if (points.length <= 0) return 0;
         Arrays.sort(points, (a, b) -> a[0] - b[0]);
-        int[] prev = points[0];
+        int prevEnd = points[0][1];
         int count = 1;
 
         for (int i = 1; i < points.length; i++) {
-            if (points[i][0] <= prev[1]) {
-                prev = new int[]{points[i][0], Math.min(points[i][1], prev[1])};
+            if (points[i][0] <= prevEnd) {
+                prevEnd = Math.min(points[i][1], prevEnd);
             } else {
-                prev = points[i];
+                prevEnd = points[i][1];
                 count++;
             }
         }
 
         return count;
     }
+
 }
