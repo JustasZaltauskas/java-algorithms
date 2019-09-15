b1f1ff2d30f3be12f07e74d15d716a35d9c552ee
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-09-15 10:58:34 +0300

    Remove duplicate algorithm

--- src/main/java/Algorithms/Uncategorized/kClosestPoints.java
+++ src/main/java/Algorithms/Uncategorized/kClosestPoints.java
@@ -23,22 +23,6 @@ public class kClosestPoints {
         return Arrays.copyOfRange(points, 0, K);
     }
 
-    public int[][] kClosest(int[][] points, int K) {
-        if (K >= points.length) return points;
-
-        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] p1, int[] p2) {
-                return (p1[0] * p1[0]) + (p1[1] * p1[1]) - (p2[0] * p2[0]) - (p2[1] * p2[1]);
-            }
-        });
-
-        for (int[] point : points) heap.offer(point);
-        int[][] result = new int[K][2];
-        for (int i = 0; i < K; i++) result[i] = heap.poll();
-
-        return result;
-    }
-
     private int getDistance(int[] point) {
         return (point[0] * point[0]) + (point[1] * point[1]);
     }
