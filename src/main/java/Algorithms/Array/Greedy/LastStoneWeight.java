0f13b5f2c435bf14f3713256a3f5ccfdae7a027b
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-09-16 17:18:38 +0300

    Delete typo

--- src/main/java/Algorithms/Array/Greedy/LastStoneWeight.java
+++ src/main/java/Algorithms/Array/Greedy/LastStoneWeight.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-C
+
 // https://leetcode.com/problems/last-stone-weight/
 public class LastStoneWeight {
     public static int find(int[] stones) {
