c1d1d7c4eda756f6592dc26717dcdafb0a0f0b4d
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-09-22 18:00:55 +0300

    Add algorithm: Binary search tree

--- src/main/java/DataStructures/BinaryTree/BinarySearchTree.java
+++ src/main/java/DataStructures/BinaryTree/BinarySearchTree.java
@@ -1,4 +1,91 @@
 package DataStructures.BinaryTree;
 
+import java.util.Collection;
+
 public class BinarySearchTree {
+    public Node<Integer> root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public BinarySearchTree(int key) {
+        this();
+        insert(key);
+    }
+
+    public BinarySearchTree(Collection<Integer> c) {
+        for (int key : c) insert(key);
+    }
+
+    public int peek() {
+        return root == null ? null : root.val;
+    }
+
+    public void insert(int key) {
+        if (root == null) {
+            root = new Node(key);
+        } else {
+            insert(root, key);
+        }
+    }
+
+    private void insert(Node<Integer> node, int key) {
+        if (node.val >= key) {
+            if (node.left == null) node.left = new Node(key);
+            else insert(node.left, key);
+        } else {
+            if (node.right == null) node.right = new Node(key);
+            else insert(node.right, key);
+        }
+    }
+
+    public Node delete(int key) {
+        return delete(root, key);
+    }
+
+    private Node delete(Node<Integer> node, int key) {
+        if (node == null) return node;
+        if (node.val > key) {
+            node.left = delete(node.left, key);
+        } else if (node.val < key){
+            node.right = delete(node.right, key);
+        } else {
+            // If node have only one child, assign it to parent node
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            }
+
+            // Assign min value from right subtree
+            node.val = minValue(node.right);
+            // Delete min value on the right subtree
+            node.right = delete(node.right, node.val);
+        }
+
+        return node;
+    }
+
+    private int minValue(Node<Integer> node) {
+        if (node.left == null) return node.val;
+        return minValue(node.left);
+    }
+
+    public boolean search(int key) {
+        return search(root, key);
+    }
+
+    private boolean search(Node<Integer> node, int key) {
+        if (node == null) return false;
+        if (node.val == key) return true;
+        if (node.val > key) return search(node.left, key);
+        else return search(node.right, key);
+    }
+
+    private Node searchNode(Node<Integer> node, int key) {
+        if (node == null || node.val == key) return node;
+        if (node.val > key) return searchNode(node.left, key);
+        else return searchNode(node.right, key);
+    }
 }
--- src/test/java/DataStructures/BinarySearchTreeTest.java
+++ src/test/java/DataStructures/BinarySearchTreeTest.java
@@ -1,4 +1,82 @@
 package DataStructures;
 
+import DataStructures.BinaryTree.BinarySearchTree;
+import DataStructures.BinaryTree.Node;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class BinarySearchTreeTest {
+    @Test
+    public void InitiateWithCollectionTest() {
+        BinarySearchTree tree = new BinarySearchTree(
+                new ArrayList<>() {{
+                    add(1);
+                    add(2);
+                    add(3);
+                }}
+        );
+
+        assertEquals("Wrong binary search tree head node", 1, tree.peek());
+    }
+
+    @Test
+    public void SeachTest() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+
+        assertTrue("Wrong binary search tree search outcome", tree.search(1));
+        assertTrue("Wrong binary search tree search outcome", tree.search(2));
+        assertTrue("Wrong binary search tree search outcome", tree.search(3));
+        assertFalse("Wrong binary search tree search outcome", tree.search(4));
+    }
+
+    @Test
+    public void DeleteNodeWith2ChildrenTest() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(11);
+        tree.insert(20);
+
+        Node node = tree.delete(10);
+        assertFalse("Binary search tree node should have been deleted" , tree.search(10));
+        assertEquals("Wrong binary search tree node after deletion", 11 , node.val);
+        assertEquals("Wrong binary search tree node after deletion", null , node.right.left);
+    }
+
+    @Test
+    public void DeleteNodeWithNoChildrenTest() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(11);
+        tree.insert(20);
+
+        Node node = tree.delete(11);
+        assertFalse("Binary search tree node should have been deleted" , tree.search(11));
+        assertEquals("Wrong binary search tree node after deletion", 15 , node.right.val);
+        assertEquals("Wrong binary search tree node after deletion", null , node.right.left);
+    }
+
+    @Test
+    public void DeleteNodeWith1ChildrenTest() {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(15);
+        tree.insert(11);
+        tree.insert(20);
+
+        Node node = tree.delete(5);
+        assertFalse("Binary search tree node should have been deleted" , tree.search(5));
+        assertEquals("Wrong binary search tree node after deletion", 6 , node.left.val);
+    }
 }
