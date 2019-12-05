b177a4d3ae7eb861f2c540b01e162a9dadc7334a
JustasZaltauskas <zaltauskasjustas@gmail.com>
2019-12-05 21:02:42 +0200

    Add memoization to TargetSum algorithm

--- src/main/java/Algorithms/DynamicProgramming/TargetSum.java
+++ src/main/java/Algorithms/DynamicProgramming/TargetSum.java
@@ -3,12 +3,17 @@ package Algorithms.DynamicProgramming;
 // https://leetcode.com/problems/target-sum/
 public class TargetSum {
     public static int findTargetSumWays(int[] nums, int S) {
-        return find(nums, S, 0);
+        Integer[][] memo = new Integer[nums.length][2001];
+        return find(nums, S, 0, 0, memo);
     }
-
-    private static int find(int[] nums, int S, int i) {
-        if (i == nums.length) return S == 0 ? 1 : 0;
-        return find(nums, S + nums[i], i + 1) + 
-            find(nums, S - nums[i], i + 1);
+    
+    public static int find(int[] nums, int S, int i, int sum, Integer[][] memo) {
+        if (i >= nums.length) return sum == S ? 1 : 0;
+        if (memo[i][sum + 1000] != null) return memo[i][sum + 1000];
+        
+        memo[i][sum + 1000] = find(nums, S, i + 1, sum + nums[i], memo) +
+            find(nums, S, i + 1, sum - nums[i], memo);
+        
+        return memo[i][sum + 1000];
     }
 }
